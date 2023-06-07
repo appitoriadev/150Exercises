831f415a7eba773e9cb69dec37a2625d9d8d7240
Laura Velez <laura.velez@globant.com>
2023-06-07 09:01:17 -0500

    created project

---
+++ SolveExercises/SolveExercises/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SolveExercises.Solve60;
+var sixty = new Exercises60();
+Console.WriteLine("Init");
+
+sixty.SumNumbers(5, 62);
+
+Console.ReadKey();
