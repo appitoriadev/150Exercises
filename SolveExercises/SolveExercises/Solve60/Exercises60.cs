17d4648ba626f30db24967a6c65424f6ae5b1238
Laura Velez <laura.velez@globant.com>
2023-06-07 09:01:34 -0500

    started working on first 60 exercises

---
+++ SolveExercises/SolveExercises/Solve60/Exercises60.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveExercises.Solve60
+{
+    public class Exercises60
+    {
+        public Exercises60() { }
+
+        public void HelloName(string _name)
+        {
+            Console.WriteLine("Hello " + _name + "!!");
+        }
+
+        public void SumNumbers(double a, double b)
+        {
+            var sum = a + b;
+            Console.WriteLine($"The sum of {a} + {b} is: " + Convert.ToString(sum) + ".");
+        }
+    }
+}
